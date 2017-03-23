fb2cd5555fe283ec3f63f0ef339b3f3ec567d410
zhaoyafeiwork <zhaoyafeiwork@sina.com>
2017-03-23 13:28:24 +0800

    提交

---
+++ src/main/java/com/siyue/test.java
@@ -0,0 +1,10 @@
+package com.siyue;
+
+/**
+ * Created by Administrator on 2017/3/23.
+ */
+public class test {
+    public static void main(String[] args) {
+        System.out.println("创建项目");
+    }
+}
